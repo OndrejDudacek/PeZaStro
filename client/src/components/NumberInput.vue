8653d13b2269f78cce56bf3f9e4506193b596777
OndrejDudacek <ondrej.dudacek@gmail.com>
2026-01-16 11:21:25 +0100

    feat: add NumberInput component with icon support and styling

---
+++ client/src/components/NumberInput.vue
@@ -0,0 +1,75 @@
+<template>
+	<section>
+		<label :for="inputName"></label>
+		<div @click="focusInput">
+			<Icon :icon-name="iconName"></Icon>
+			<input
+				ref="inputRef"
+				type="number"
+				:name="inputName"
+				:placeholder="placeholder"
+				:value="modelValue"
+				@input="emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+			/>
+		</div>
+	</section>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import Icon from "./Icon.vue";
+
+const props = defineProps({
+	iconName: {
+		type: String,
+	},
+	inputName: {
+		type: String,
+	},
+	placeholder: {
+		type: String,
+	},
+	modelValue: {
+		type: Number,
+	},
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
+const inputRef = ref<HTMLInputElement | null>(null);
+
+const focusInput = () => {
+	inputRef.value?.focus();
+};
+</script>
+
+<style scoped lang="scss">
+div {
+	display: flex;
+	justify-content: left;
+	align-items: center;
+	gap: var(--spacing-xs);
+	background-color: var(--color-background);
+	cursor: pointer;
+	padding: var(--spacing-s);
+	outline: var(--border-width) solid var(--color-border);
+	border-radius: var(--border-radius);
+
+	input {
+		font: inherit;
+		background-color: inherit;
+		outline: none;
+		border: none;
+		width: 100%;
+	}
+}
+
+div:hover {
+	outline: var(--border-width-hover) solid var(--color-border);
+}
+
+div:active,
+div:has(input:focus) {
+	outline: var(--border-width-active) solid var(--color-border);
+}
+</style>
