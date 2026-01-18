40972dee4637af55da1afd42a7aab03b8687b207
OndrejDudacek <ondrej.dudacek@gmail.com>
2026-01-18 17:26:12 +0100

    feat: add DetailCustomer component for displaying customer details and contacts

---
+++ client/src/components/DetailCustomer.vue
@@ -0,0 +1,60 @@
+<template>
+	<article>
+		<h3>Detail zákazníka</h3>
+		<ul>
+			<li>
+				<p>Id: {{}}</p>
+			</li>
+			<li>
+				<p>Datum vytvoření: {{}}</p>
+			</li>
+			<li>
+				<TextInput label="Jméno:" placeholder="Jan Nezdar" />
+			</li>
+			<li>
+				<TextArea label="Poznámky:" />
+			</li>
+		</ul>
+		<h4>Kontakty:</h4>
+		<ul>
+			<!-- v-for="(customers, i) in customer" :key="i" -->
+			<li>
+				<ul>
+					<li>
+						<p>Id: {{}}</p>
+					</li>
+					<li>
+						<p>Datum vytvoření: {{}}</p>
+					</li>
+					<li>
+						<TextInput label="Jméno:" placeholder="Josefína Nezdarová" />
+					</li>
+					<li>
+						<NumberInput label="Telefon:" placeholder="777666111" />
+					</li>
+					<li>
+						<TextInput label="Email:" placeholder="josefina.nezdarova@gmail.com" />
+					</li>
+				</ul>
+			</li>
+		</ul>
+		<Button label="Přidat kontakt" icon="add" />
+		<Button label="Odeslat změny" icon="upload"></Button>
+	</article>
+</template>
+
+<script setup lang="ts">
+import Button from "./Button.vue";
+import NumberInput from "./NumberInput.vue";
+import TextInput from "./TextInput.vue";
+import TextArea from "./TextArea.vue";
+
+const props = defineProps<{
+	customerId: String;
+}>();
+</script>
+
+<style scoped lang="scss">
+@use "../assets/mixins" as *;
+@include detailStyling;
+</style>
