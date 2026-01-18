7ab1a53cb7b896f416b72ba7d05631f374beb918
OndrejDudacek <ondrej.dudacek@gmail.com>
2026-01-18 17:26:21 +0100

    feat: add DetailJob, DetailLocation, and DashboardView components with initial templates

---
+++ client/src/components/DetailLocation.vue
@@ -0,0 +1,45 @@
+<template>
+	<article>
+		<h3>Detail lokace</h3>
+		<ul>
+			<li>
+				<p>Id: {{}}</p>
+			</li>
+			<li>
+				<p>Datum vytvoření: {{}}</p>
+			</li>
+			<li>
+				<TextInput label="Ulice:" placeholder="Rohová" />
+			</li>
+			<li>
+				<NumberInput label="Číslo domu:" placeholder="559" />
+			</li>
+			<li>
+				<TextInput label="PSČ:" placeholder="151 00" />
+			</li>
+			<li>
+				<TextInput label="Město:" placeholder="Praha" />
+			</li>
+			<li>
+				<TextArea label="Poznámky:" />
+			</li>
+		</ul>
+		<Button label="Odeslat změny" icon="upload"></Button>
+	</article>
+</template>
+
+<script setup lang="ts">
+import Button from "./Button.vue";
+import NumberInput from "./NumberInput.vue";
+import TextInput from "./TextInput.vue";
+import TextArea from "./TextArea.vue";
+
+const props = defineProps<{
+	locationId: String;
+}>();
+</script>
+
+<style scoped lang="scss">
+@use "../assets/mixins" as *;
+@include detailStyling;
+</style>
---
+++ client/src/views/DashboardView.vue
@@ -0,0 +1,3 @@
+<template>
+	<h2>Dashborad</h2>
+</template>
