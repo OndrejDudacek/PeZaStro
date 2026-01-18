782cc7d4044547a85fdf022b5e7faf4e493c5684
OndrejDudacek <ondrej.dudacek@gmail.com>
2026-01-18 17:36:43 +0100

    feat: implement DetailJob component with form inputs and styling

--- client/src/components/DetailJob.vue
+++ client/src/components/DetailJob.vue
@@ -1,3 +1,43 @@
-<template></template>
-<script setup lang="ts"></script>
-<style scoped lang="scss"></style>
+<template>
+	<article>
+		<h3>Detail práce</h3>
+		<ul>
+			<li>
+				<p>Id: {{}}</p>
+			</li>
+			<li>
+				<p>Datum vytvoření: {{}}</p>
+			</li>
+			<li>
+				<p>Datum provedení:</p>
+				<div class="dateInputs">
+					<NumberInput label="Den:" />
+					<NumberInput label="Měsíc:" />
+					<NumberInput label="Rok:" />
+				</div>
+			</li>
+			<li>
+				<p>Id zakázky: {{}}</p>
+			</li>
+			<li>
+				<TextArea label="Poznámky:" />
+			</li>
+		</ul>
+		<Button label="Odeslat změny" icon="upload"></Button>
+	</article>
+</template>
+
+<script setup lang="ts">
+import Button from "./Button.vue";
+import NumberInput from "./NumberInput.vue";
+import TextArea from "./TextArea.vue";
+
+const props = defineProps<{
+	jobId: String;
+}>();
+</script>
+
+<style scoped lang="scss">
+@use "../assets/mixins" as *;
+@include detailStyling;
+</style>
