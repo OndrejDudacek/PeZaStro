e6e5701913277c6ba8d35a343e9c28a2711b286e
OndrejDudacek <ondrej.dudacek@gmail.com>
2026-02-04 08:56:18 +0100

    feat: refactor userService to improve login, logout, and retrieveUser functionality

--- client/src/services/userService.ts
+++ client/src/services/userService.ts
@@ -6,7 +6,6 @@ const source = "/user";
 
 export const userService = {
 	async login(data: UserLogin) {
-		const userStore = useUserStore();
 		try {
 			const { username, token, id } = await apiClient.post<UserLoginResponse>(
 				`${source}/login`,
@@ -16,18 +15,77 @@ export const userService = {
 				},
 			);
 
+			const userStore = useUserStore();
 			userStore.onLogin({ token, id, username });
+
+			localStorage.setItem("id", id);
+			localStorage.setItem("username", username);
+			localStorage.setItem("token", token);
+			localStorage.setItem("lastLogin", String(Date.now()));
+
 			return true;
 		} catch (error) {
+			console.error(error);
 			return false;
 		}
 	},
+
 	logout() {
 		try {
 			const userStore = useUserStore();
 			userStore.onLogout();
+
+			localStorage.removeItem("id");
+			localStorage.removeItem("username");
+			localStorage.removeItem("token");
+			localStorage.removeItem("lastLogin");
+
 			return true;
 		} catch (error) {
+			console.error(error);
+			return false;
+		}
+	},
+
+	retrieveUser() {
+		try {
+			const lastLogin = localStorage.getItem("lastLogin");
+			if (!lastLogin) {
+				return false;
+			}
+
+			const lastLoginDifference = Date.now() - Number(lastLogin);
+			if (lastLoginDifference >= 1 * 24 * 60 * 60 * 1000) {
+				localStorage.removeItem("id");
+				localStorage.removeItem("username");
+				localStorage.removeItem("token");
+				localStorage.removeItem("lastLogin");
+				return false;
+			}
+
+			const retriviedId = localStorage.getItem("id");
+			const retriviedUsername = localStorage.getItem("username");
+			const retriviedToken = localStorage.getItem("token");
+
+			if (!retriviedId || !retriviedUsername || !retriviedToken) {
+				return false;
+			}
+
+			const userStore = useUserStore();
+			userStore.onLogin({
+				token: retriviedToken,
+				id: retriviedId,
+				username: retriviedUsername,
+			});
+
+			localStorage.setItem("id", retriviedUsername);
+			localStorage.setItem("username", retriviedUsername);
+			localStorage.setItem("token", retriviedUsername);
+			localStorage.setItem("lastLogin", String(Date.now()));
+
+			return true;
+		} catch (error) {
+			console.error(error);
 			return false;
 		}
 	},
