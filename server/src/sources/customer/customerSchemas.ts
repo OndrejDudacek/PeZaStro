29ef2c22ccfcce393fe4f9a8051b399c1fc76614
OndrejDudacek <ondrej.dudacek@gmail.com>
2026-02-10 21:33:47 +0100

    fix: update customer schemas to allow null for note field

--- server/src/sources/customer/customerSchemas.ts
+++ server/src/sources/customer/customerSchemas.ts
@@ -3,10 +3,10 @@ import { idSchema } from "../../utils/idSchema.js";
 
 export const CreateCustomerSchema = Joi.object({
 	name: Joi.string().min(3).max(50).required(),
-	note: Joi.string().max(1500).optional(),
+	note: Joi.string().max(1500).allow(null).optional(),
 });
 
 export const UpdateCustomerSchema = Joi.object({
 	name: Joi.string().min(3).max(50).optional(),
-	note: Joi.string().max(1500).optional(),
+	note: Joi.string().max(1500).allow(null).optional(),
 });
