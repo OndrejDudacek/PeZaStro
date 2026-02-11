8054979eadd55ef262410565fbff06ba9b16193f
OndrejDudacek <ondrej.dudacek@gmail.com>
2026-02-11 14:35:27 +0100

    feat: update contact schemas to allow null values for phone and email fields

--- server/src/sources/contact/contactSchemas.ts
+++ server/src/sources/contact/contactSchemas.ts
@@ -5,14 +5,14 @@ export const CreateContactSchema = Joi.object({
 	name: Joi.string().min(3).max(50).required(),
 	customerId: idSchema.required(),
 	// customerId: Joi.string().required(),
-	phone: Joi.number().optional(),
-	email: Joi.string().email().optional(),
+	phone: Joi.number().allow(null).optional(),
+	email: Joi.string().email().allow(null).optional(),
 });
 
 export const UpdateContactSchema = Joi.object({
 	name: Joi.string().min(3).max(50).optional(),
 	customerId: idSchema.optional(),
 	// customerId: Joi.string().optional(),
-	phone: Joi.number().optional(),
-	email: Joi.string().email().optional(),
+	phone: Joi.number().allow(null).optional(),
+	email: Joi.string().email().allow(null).optional(),
 });
--- server/src/sources/job/jobSchemas.ts
+++ server/src/sources/job/jobSchemas.ts
@@ -4,7 +4,7 @@ import { idSchema } from "../../utils/idSchema.js";
 export const CreateJobSchema = Joi.object({
 	date: Joi.string().isoDate().required(),
 	contractId: idSchema.required(),
-	note: Joi.string().max(1500).allow(null).required(),
+	note: Joi.string().max(1500).required(),
 });
 
 export const UpdateJobSchema = Joi.object({
